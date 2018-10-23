a95749dc25d01d56b20e4ecb9b922b249a01fa44
Guordek <30309206+Guordek@users.noreply.github.com>
2018-10-23 15:39:24 +0200

    add test method EdgeKirsch, EdgeLaplacian5x5 and EdgeSobel3x3

--- ImageEdgeDetectionTest/ImageEdgeDetectionTest.cs
+++ ImageEdgeDetectionTest/ImageEdgeDetectionTest.cs
@@ -35,8 +35,62 @@ namespace ImageEdgeDetectionTest
 
             bmp1 = ImageFilters.BlackWhite(bmp1);
             Assert.IsTrue(CompareBitmaps(bmp1, bmp2));
-        }     
-        
+        }
+
+
+        [TestMethod]
+        public void EdgeKirschFilterTest()
+        {
+
+            var projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            StreamReader streamReader = new StreamReader(Path.Combine(projectFolder, @"data\edged middle.png"));
+            Bitmap bmp1 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            streamReader = new StreamReader(Path.Combine(projectFolder, @"data\edged middle kirsch.png"));
+            Bitmap bmp2 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            bmp1 = ExtBitmap.KirschFilter(bmp1);
+            Assert.IsTrue(CompareBitmaps(bmp1, bmp2));
+        }
+
+        [TestMethod]
+        public void EdgeLaplacian5x5FilterTest()
+        {
+
+            var projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            StreamReader streamReader = new StreamReader(Path.Combine(projectFolder, @"data\edged middle.png"));
+            Bitmap bmp1 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            streamReader = new StreamReader(Path.Combine(projectFolder, @"data\edged middle laplacian 5x5.png"));
+            Bitmap bmp2 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            bmp1 = ExtBitmap.Laplacian5x5Filter(bmp1);
+            Assert.IsTrue(CompareBitmaps(bmp1, bmp2));
+        }
+
+        [TestMethod]
+        public void EdgeSobel3x3FilterTest()
+        {
+
+            var projectFolder = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            StreamReader streamReader = new StreamReader(Path.Combine(projectFolder, @"data\edged middle.png"));
+            Bitmap bmp1 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            streamReader = new StreamReader(Path.Combine(projectFolder, @"data\edged middle sobel 3x3.png"));
+            Bitmap bmp2 = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+
+            bmp1 = ExtBitmap.Sobel3x3Filter(bmp1);
+            Assert.IsTrue(CompareBitmaps(bmp1, bmp2));
+        }
 
         public bool CompareBitmaps(Bitmap bmp1, Bitmap bmp2)
         {
